08028b9f21a784ffeea22d9724d000d71cfec3e4
MaxCapette <sk8baz@gmail.com>
2023-08-18 13:57:10 +0200

    filter ok

--- src/components/Currencies/Currencies.tsx
+++ src/components/Currencies/Currencies.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ICurrency } from '../../@types/converter';
 import './Currencies.scss';
 
@@ -5,11 +6,25 @@ interface LiCurrency {
   currencies: ICurrency[];
 }
 function Currencies({ currencies }: LiCurrency) {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+  const filteredCurrencies = currencies.filter((currency) =>
+    currency.description.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="scrollLi">
-      <h2>Currencies</h2>
+      <input
+        type="text"
+        className="currencies-input"
+        placeholder="search currencies..."
+        onChange={handleChange}
+        value={search}
+      />
       <ul>
-        {currencies.map((currency) => {
+        {filteredCurrencies.map((currency) => {
           return <li key={currency.code}>{currency.description}</li>;
         })}
       </ul>
