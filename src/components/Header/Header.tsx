43c323df2a3be5f0b4ed43ec959759bfad011ff0
MaxCapette <sk8baz@gmail.com>
2023-08-18 18:30:10 +0200

    gestions des erreurs de saisies ❤️

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 // on importe la feuille de style propre au composant
 // chaque composant doit etre indépendant donc il possede son propre style
-
+import React, { useState } from 'react';
 import './Header.scss';
 
 interface IHeaderProps {
@@ -9,20 +9,30 @@ interface IHeaderProps {
 }
 
 function Header({ amount, setNbInput }: IHeaderProps) {
+  const [inputError, setInputError] = useState(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const newValue = Number(value);
+    if (!Number.isNaN(newValue) || value === '') {
+      setInputError(false);
+      setNbInput(newValue);
+    } else {
+      setInputError(true);
+    }
+  };
   return (
     <header className="Header">
       <h1 className="Header-title">Converter</h1>
       <input
         type="text"
-        onChange={(event) => {
-          const newValue = Number(event.target.value);
-          if (!Number.isNaN(newValue)) {
-            setNbInput(newValue);
-          }
-        }}
+        className={inputError ? 'error' : ''}
+        onChange={handleChange}
         value={amount}
         placeholder="Enter the amount"
       />
+      {inputError && (
+        <div className="error-message">Please enter a valid number.</div>
+      )}
     </header>
   );
 }
